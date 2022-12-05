3a3eaed415c12cb816402f3611435b2194eb3991
art848 <113240700+art848@users.noreply.github.com>
2022-12-05 20:19:33 +0400

    Update UrlService.js

--- src/services/UrlService.js
+++ src/services/UrlService.js
@@ -2,21 +2,39 @@ import fetch from 'node-fetch';
 
 class UrlService {
   static async checkUrls(urls) {
+    const informationalResponses = [];
+    const successfulResponses = [];
+    const redirectionMessages = [];
+    const clientErrorResponses = [];
+
     const items = urls.map((ur) => (`https://${ur.domain}`));
     const ids = urls.map((id) => id.id);
-    const res = await Promise.allSettled(items.map((url) => fetch(url)));
+    await Promise.allSettled(items.map((url) => fetch(url)))
+      .then((res) => {
+        for (let index = 0; index < res.length; index += 1) {
+          if (res[index].status === 'fulfilled') {
+            if (res[index].value.status >= 100 && res[index].value.status < 200) {
+              informationalResponses.push(ids[index]);
+            } else if (res[index].value.status >= 200 && res[index].value.status < 300) {
+              successfulResponses.push(ids[index]);
+            } else if (res[index].value.status >= 300 && res[index].value.status < 400) {
+              redirectionMessages.push(ids[index]);
+            } else if (res[index].value.status >= 400 && res[index].value.status < 500) {
+              clientErrorResponses.push(ids[index]);
+            }
+          } else if (res[index].status === 'rejected') {
+            clientErrorResponses.push(ids[index]);
+          }
+        }
+      });
+    // console.log('informationalResponses =>', informationalResponses);
+    // console.log('successfulResponses =>', successfulResponses);
+    // console.log('redirectionMessages =>', redirectionMessages);
+    // console.log('clientErrorResponses =>', clientErrorResponses);
 
-    const fulfilledUrl = [];
-    const rejectedUrl = [];
-    for (let i = 0; i < ids.length; i += 1) {
-      if (res[i].status !== 'fulfilled') {
-        rejectedUrl.push(ids[i]);
-      } else {
-        fulfilledUrl.push(ids[i]);
-      }
-    }
-    return [rejectedUrl, fulfilledUrl];
+    return [informationalResponses, successfulResponses, redirectionMessages, clientErrorResponses];
   }
 }
 
 export default UrlService;
+
